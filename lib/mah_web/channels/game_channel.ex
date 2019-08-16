84d43724d2bd743417f92c8421521cdfce6ae0e1
Joe-noh <goflb.jh@gmail.com>
2019-08-16 17:57:59 +0900

    channel layer no longer need to wait 100ms

--- lib/mah_web/channels/game_channel.ex
+++ lib/mah_web/channels/game_channel.ex
@@ -3,24 +3,25 @@ defmodule MahWeb.GameChannel do
 
   def join("game:" <> game_id, _payload, socket) do
     send(self(), :track_presence)
-    Process.send_after(self(), {:start_game, game_id}, 100)
 
     {:ok, assign(socket, :game_id, game_id)}
+  end
+
+  def handle_in("ready", _, socket = %{assigns: %{user_id: user_id, game_id: game_id}}) do
+    if {:ok, :startable} == Mah.Game.player_ready(game_id, user_id) do
+      players = MahWeb.Presence.list(socket) |> Map.keys()
+
+      if Mah.Game.startable_with?(game_id, players) do
+        Mah.Game.start_game(game_id)
+      end
+    end
+
+    {:noreply, socket}
   end
 
   def handle_info(:track_presence, socket) do
     push(socket, "presence_state", MahWeb.Presence.list(socket))
     {:ok, _} = MahWeb.Presence.track(socket, socket.assigns.user_id, %{})
-
-    {:noreply, socket}
-  end
-
-  def handle_info({:start_game, game_id}, socket) do
-    players = MahWeb.Presence.list(socket) |> Map.keys()
-
-    if Mah.Game.startable_with?(game_id, players) do
-      Mah.Game.start_game(game_id)
-    end
 
     {:noreply, socket}
   end
